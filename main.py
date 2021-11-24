149a0c47cc030170efe06d70721d96a0da63c2bc
bac <bactran@gmail.com>
2021-11-24 23:36:09 +0700

    oriented-object programize

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from utils import data
-from learning.regression import linear
+from learning.regression.linear import Model1
 
 
 def main():
     x, y = data.read_csv('data/input.csv')
     x_train, y_train = x[:24], y[:24]
     x_test, y_test = x[24:], y[24:]
-    w = linear.train(x_train, y_train)
-    print(w)
-    print('---')
-    print(linear.predict(x_test, w))
+    model = Model1(x_train, y_train)
+    model.train()
+    print(model.predict(x_test))
     print(y_test)
 
 if __name__ == '__main__':
