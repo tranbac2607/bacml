682f036c65fde8df598f17dec2bb44b1f9680e4a
bac <bactran@gmail.com>
2021-11-24 23:52:43 +0700

    pass filepath

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from utils import data
 from learning.regression import linear
+import sys
 
 
-def main():
-    x, y = data.read_csv('data/input.csv')
+def main(filename):
+    x, y = data.read_csv(filename)
     x_train, y_train = x[:24], y[:24]
     x_test, y_test = x[24:], y[24:]
     w = linear.train(x_train, y_train)
@@ -13,4 +14,5 @@ def main():
     print(y_test)
 
 if __name__ == '__main__':
-    main()
+    if (len(sys.argv) > 1): main(sys.argv[1])
+    else: print('Please enter file path!')
